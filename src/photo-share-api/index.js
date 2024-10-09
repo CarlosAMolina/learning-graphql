9d13271e0b2cf5311b570fc566d26a3656435415
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-09 23:32:30 +0200

    fix: start api

--- src/photo-share-api/index.js
+++ src/photo-share-api/index.js
@@ -1,7 +1,8 @@
 const { ApolloServer } = require('apollo-server-express')
+const { GraphQLScalarType } = require('graphql')
+const { readFileSync } = require('fs')
 const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
-const { GraphQLScalarType } = require('graphql')
 
 var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 
@@ -112,9 +113,14 @@ const server = new ApolloServer({
     resolvers
 })
 
-server.applyMiddleware({ app })
-app.get('/', (req,res) => res.end('Welcome to the PhotoShare API'))
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
-app.listen({ port: 4000 }, () =>
-    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-)
+async function start() {
+    await server.start()
+    server.applyMiddleware({ app })
+    app.get('/', (req,res) => res.end('Welcome to the PhotoShare API'))
+    app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+    app.listen({ port: 4000 }, () =>
+        console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+    )
+}
+
+start()
